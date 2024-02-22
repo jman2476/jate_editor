a3dea243999cc3bf99d9ae730e6c32a1d3121f92
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-02-22 02:39:23 -0500

    added database interactions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,19 @@ const initdb = async () =>
     },
   });
 
+// create transaction to update the database
+const transact = initdb.transaction('jate', 'readwrite')
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  await initdb.put('jate', content, 1)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // jate get index of 1
+  const text = await initdb.get('jate', 1)
+  return text
+}
 
 initdb();
