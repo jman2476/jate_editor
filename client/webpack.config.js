69018c558edf67cd7cf94ad5baba2e8cbaca3cba
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-02-22 23:50:00 -0500

    added a bunch of things with the help of my TA Zack, including idb stuff and finishing the webpack config file, as well as running scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: "JATE",
+        template: path.join(__dirname, './index.html')
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        theme_color: '#fff',
+        publicPath: '/',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "ie 9" }]
+              ]
+            }
+          }
+        }
       ],
     },
   };
